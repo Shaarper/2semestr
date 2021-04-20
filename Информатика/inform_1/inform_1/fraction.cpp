5ce293bc284ab3b7c0eda881fcfb2ce9483894ce
Shaarper <65732043+Shaarper@users.noreply.github.com>
2021-04-20 18:17:23 +0500

    Delete Информатика/inform_1 directory

--- Информатика/inform_1/inform_1/fraction.cpp
+++
@@ -1,9 +0,0 @@
-struct fraction
-{
-	double first;
-	int second;
-	void Init(double, int); //метод для инициализации полей
-	void Read(); //метод для чтения значений полей
-	void Show(); //метод для вывода значений полей
-	double Power(); //вычисление степени
-};
